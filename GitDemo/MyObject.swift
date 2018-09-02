8d51fe94efd2e8c5b19ab74c2a4262b9036d41cc
Jigar Jarsania <JigarJarsania@jigars-macbook-air.local>
2018-09-02 12:50:43 +0530

    new file commit

---
+++ GitDemo/MyObject.swift
@@ -0,0 +1,13 @@
+//
+//  MyObject.swift
+//  GitDemo
+//
+//  Created by Jigar Jarsania on 9/2/18.
+//  Copyright © 2018 Sneha Jarsania. All rights reserved.
+//
+
+import UIKit
+
+class MyObject: NSObject {
+
+}
